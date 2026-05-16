6aea4a0d2516e7f16b8cd0b6ac9739ec0c20aee0
@tigrao <romastefale@gmail.com>
2026-05-16 13:21:31 -0300

    Include registered routers

--- app/app_factory.py
+++ app/app_factory.py
@@ -5,4 +5,6 @@ from app.routes import ROUTERS
 
 def create_dispatcher() -> Dispatcher:
     dispatcher = Dispatcher()
+    for route in ROUTERS:
+        dispatcher.include_router(route)
     return dispatcher
