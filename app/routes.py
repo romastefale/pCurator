5e397a64eb79ed3251047b7d759777c71a15dd56
@tigrao <romastefale@gmail.com>
2026-05-16 14:05:10 -0300

    Register manual image flow

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app.callbacks import router as callback_router
 from app.commands import router as commands_router
 from app.edit_flow import router as edit_router
+from app.image_flow import router as image_router
 from app.link_flow import router as link_router
 
-ROUTERS = (commands_router, edit_router, link_router, callback_router)
+ROUTERS = (commands_router, image_router, edit_router, link_router, callback_router)
