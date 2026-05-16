eec93e1cbf3cda343146bba234e4151359d894d2
@tigrao <romastefale@gmail.com>
2026-05-16 13:39:45 -0300

    Register callback router

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
+from app.callbacks import router as callback_router
 from app.commands import router as commands_router
 from app.link_flow import router as link_router
 
-ROUTERS = (commands_router, link_router)
+ROUTERS = (commands_router, link_router, callback_router)
