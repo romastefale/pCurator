606e330384c3affe2bc453adfa636e5d2e2db2b4
@tigrao <romastefale@gmail.com>
2026-05-16 14:02:42 -0300

    Register manual edit flow

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app.callbacks import router as callback_router
 from app.commands import router as commands_router
+from app.edit_flow import router as edit_router
 from app.link_flow import router as link_router
 
-ROUTERS = (commands_router, link_router, callback_router)
+ROUTERS = (commands_router, edit_router, link_router, callback_router)
